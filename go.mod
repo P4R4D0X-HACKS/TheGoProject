7cd9886a5fd9077edd0c0dc224432c403b56469f
P4R4D0X-HACKS <paradox_hacks@protonmail.com>
2024-10-03 14:06:26 +0530

    nmap using go

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/P4R4D0X-HACKS/TheGoProject
 
 go 1.22.1
+
+require github.com/Ullaakut/nmap v2.0.2+incompatible // indirect
---
+++ go_lang_hacking/network_scanner/nmap/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/Ullaakut/nmap"
+)
+
+func main() {
+	targetIP := "scanme.nmap.org"
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	scanner, err := nmap.NewScanner(
+		nmap.WithTargets(targetIP),
+		nmap.WithPorts("80", "443"),
+		nmap.WithContext(ctx),
+	)
+	if err != nil {
+		log.Fatalf("error creating scanner: %v", err)
+	}
+
+	result, warnings, err := scanner.Run()
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+
+	if warnings != nil {
+		log.Printf("warnings: \n%v", warnings)
+	}
+
+	for _, host := range result.Hosts {
+		if len(host.Ports) == 0 || len(host.Addresses) == 0 {
+			continue
+		}
+
+		fmt.Printf("Host %q:\n", host.Addresses[0])
+		if len(host.Addresses) > 1 {
+			fmt.Printf("  - MAC addresses: %v\n", host.Addresses[1:])
+		}
+	}
+}
