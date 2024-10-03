2fb6314b005e93eec71009422783cc9cce9483e2
P4R4D0X-HACKS <paradox_hacks@protonmail.com>
2024-10-03 13:43:58 +0530

    Refactor network scanner to use goroutines for concurrent port scanning

--- go_lang_hacking/network_scanner/main.go
+++ go_lang_hacking/network_scanner/main.go
@@ -3,24 +3,34 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 	"time"
 )
 
-func main() {
+func scanPort(port int, wg *sync.WaitGroup) {
+	defer wg.Done()
 	IP := "scanme.nmap.org"
-
-	for i := 1; i < 100; i++ {
-		address := fmt.Sprintf(IP+":%d", i)
-		
-		// Adding a timeout for connection attempts to avoid hanging
-		conn, err := net.DialTimeout("tcp", address, 2*time.Second)
-		
-		if err == nil {
-			fmt.Printf("[+] Connection Established... PORT %v\n", i)
-			// Close the connection immediately after establishing it
-			conn.Close()
-		} else {
-			fmt.Printf("[-] Connection Failed on PORT %v\n", i)
-		}
+	address := fmt.Sprintf(IP+":%d", port)
+	
+	// Adding a timeout for connection attempts to avoid hanging
+	conn, err := net.DialTimeout("tcp", address, 2*time.Second)
+	
+	if err == nil {
+		fmt.Printf("[+] Connection Established... PORT %v\n", port)
+		// Close the connection immediately after establishing it
+		conn.Close()
+	} else {
+		fmt.Printf("[-] Connection Failed on PORT %v\n", port)
 	}
+}
+
+func main() {
+
+	var wg sync.WaitGroup
+	// Scan the first 1024 ports
+	for i := 1; i <= 1024; i++ {
+		wg.Add(1)
+		go scanPort(i, &wg)
+	}
+	wg.Wait()
 }
