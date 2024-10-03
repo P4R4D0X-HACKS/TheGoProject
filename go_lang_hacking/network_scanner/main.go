51b336e07e1d51ac6da7fca6287adf3cd0100b54
P4R4D0X-HACKS <paradox_hacks@protonmail.com>
2024-10-03 13:14:21 +0530

    update

--- go_lang_hacking/network_scanner/main.go
+++ go_lang_hacking/network_scanner/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net"
+	"time"
 )
 
 func main() {
@@ -10,10 +11,13 @@ func main() {
 
 	for i := 1; i < 100; i++ {
 		address := fmt.Sprintf(IP+":%d", i)
-		conn, err := net.Dial("tcp", address)
+		
+		// Adding a timeout for connection attempts to avoid hanging
+		conn, err := net.DialTimeout("tcp", address, 2*time.Second)
+		
 		if err == nil {
 			fmt.Printf("[+] Connection Established... PORT %v\n", i)
-			// Close the connection after use
+			// Close the connection immediately after establishing it
 			conn.Close()
 		} else {
 			fmt.Printf("[-] Connection Failed on PORT %v\n", i)
